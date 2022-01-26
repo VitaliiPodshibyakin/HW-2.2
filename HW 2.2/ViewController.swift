7abfced5ba70f38e6daf33fa0f09e844b90e6692
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2022-01-26 19:40:41 +0300

    Setup Logic

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
-
 class ViewController: UIViewController {
-
+    @IBOutlet var viewToColor: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //View
+        viewToColor.layer.cornerRadius = 10
+       
+        //Slider
+        redSlider.tintColor = .red
+        greenSlider.tintColor = .green
+        blueSlider.tintColor = .blue
+        
     }
-
-
+    
+    override func viewWillLayoutSubviews() {
+        let redSliderValue = CGFloat(redSlider.value)
+        let greenSliderValue = CGFloat(greenSlider.value)
+        let blueSliderValue = CGFloat(blueSlider.value)
+        let viewColor = UIColor(displayP3Red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+        viewToColor.backgroundColor = viewColor
+    }
+    
+    @IBAction func redSliderColoring() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        
+    }
+    
+    @IBAction func greenSliderColoring() {
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func blueSliderColoring() {
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
